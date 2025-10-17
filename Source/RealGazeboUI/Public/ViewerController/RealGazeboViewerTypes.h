924a8ab4ec7e81972c2cf73a6c91d1e94764e9e7
Hiru-93 <sandaruwan.lahiru1993@gmail.com>
2025-10-18 00:40:12 +0900

    Update RealGazeboUI Module

--- Source/RealGazeboUI/Public/ViewerController/RealGazeboViewerTypes.h
+++ Source/RealGazeboUI/Public/ViewerController/RealGazeboViewerTypes.h
@@ -1,6 +1,5 @@
 // Copyright (c) 2024-2025 SUV Lab, Chungbuk National University
 // Author    : Gonapinuwala Lahiru Sandaruwan
-// Sub-author: MinKyu Kim
 // Supervisor: Prof. SungTae Moon - Project lead & research supervision
 // Licensed under the BSD-3-Clause License.
 // See LICENSE file in the project root for full license information.
